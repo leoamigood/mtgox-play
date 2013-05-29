50e23f3633cf5ca4f93a3849c5b90c4e7deaff12
Leo Amigood <leo@1stdibs.com>
2013-05-28 22:10:14 -0400

    Converting json-lift to json-play via full serialization

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,27 +7,13 @@ import akka.dispatch._
 import java.util.concurrent.Executors
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def npe = Action {
-
-    implicit val ec = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
-
-    val f = Future {
-      2 / 0
-    }
-
-    f onFailure {
-      case npe: NullPointerException =>
-        println("Amaized!")
-      case ex: Exception =>
-        println("Amaized!")
-    }
-
-    Ok(views.html.index("Your new application is ready, NPE."))
+  def ticker = Action {
+    Ok(views.html.ticker("Your new application is ready, ticker"))
   }
   
 }
